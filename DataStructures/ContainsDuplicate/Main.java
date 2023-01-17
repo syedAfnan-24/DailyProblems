e5041ece271c0d80cbe5857f18670665787de459
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-01-17 15:39:19 +0530

    Day #1

---
+++ DataStructures/ContainsDuplicate/Main.java
@@ -0,0 +1,10 @@
+package leetCode.DataStructures.ContainsDuplicate;
+
+public class Main {
+	public static void main(String args[]) {
+		Solution ob = new Solution();
+		int nums[]= {1,2,3,1};
+		System.out.println(ob.containsDuplicate(nums));
+	}
+	
+}
