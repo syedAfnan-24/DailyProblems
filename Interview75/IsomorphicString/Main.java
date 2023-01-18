ba4423ee130aef88025df498cbc2aae6c73e58e6
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-01-18 13:23:59 +0530

    Day #2

---
+++ Interview75/IsomorphicString/Main.java
@@ -0,0 +1,11 @@
+package leetCode.Interview75.IsomorphicString;
+
+public class Main {
+	public static void main(String args[]) {
+		String s="paper";
+		String t="title";
+		Solution ob = new Solution();
+		System.out.println(ob.isIsomorphic(s,t));
+		
+	}
+}
