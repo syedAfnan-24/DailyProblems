d6e053e60a352780ce462fa65d297bb1f3b252f2
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-01-18 23:59:43 +0530

    Day#2

--- DataStructures/MergeSortedArray/Solution.java
+++ DataStructures/MergeSortedArray/Solution.java
@@ -1,7 +1,7 @@
 package leetCode.DataStructures.MergeSortedArray;
 
 import java.util.Arrays;
-
+//problem 88 day 2
 public class Solution {
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2,0,nums1,m,n);
