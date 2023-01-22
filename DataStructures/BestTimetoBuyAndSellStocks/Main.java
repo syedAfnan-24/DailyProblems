0907fb07fef1c73eb883398cce8c5c3ed4e86205
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-01-22 17:25:36 +0530

    Day#3

---
+++ DataStructures/BestTimetoBuyAndSellStocks/Main.java
@@ -0,0 +1,9 @@
+package leetCode.DataStructures.BestTimetoBuyAndSellStocks;
+
+public class Main {
+	public static void main(String args[]) {
+		Solution ob = new Solution();
+		int prices[] = {7,1,5,3,6,4};
+		System.out.println(ob.maxProfit(prices));
+	}
+}
